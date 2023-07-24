0c93d7cdc00312b05b73961505a3250552553c3a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-25 00:08:25 +0300

    finish create prompt page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -12,7 +12,22 @@ const CreatePrompt = () => {
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: '', tag: '' });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({ prompt: post.prompt, userId: session?.user.id, tag: post.tag }),
+      });
+      response.ok && router.push('/');
+    } catch (error) {
+      console.error('Error creating a prompt:', error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return <Form type='Create' post={post} setPost={setPost} submitting={submitting} handleSubmit={createPrompt} />;
 };
