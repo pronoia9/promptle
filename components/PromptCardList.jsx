055dd253980d64ed3afa1f374936ed7e1bbeaed7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-25 00:42:18 +0300

    finish prompt card list

--- components/PromptCardList.jsx
+++ components/PromptCardList.jsx
@@ -1,4 +1,10 @@
-const PromptCardList = () => {
-  return <div>PromptCardList</div>;
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data?.map((post) => (
+        <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  );
 };
 export default PromptCardList;
