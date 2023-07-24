d2b33b30f1e59c9abc479e7dc6985248cc9372ca
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-25 00:40:22 +0300

    add init feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,37 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { PromptCard, PromptCardList } from '.';
+
 const Feed = () => {
-  return <div>Feed</div>;
+  const [allPosts, setAllPosts] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = () => { };
+
+  const filterPrompts = () => { };
+
+  const handleSearchChange = (e) => { };
+  
+  const handleTagClick = () => { };
+  
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList data={searchText ? searchedResults : allPosts} handleTagClick={handleTagClick} />
+    </section>
+  );
 };
 export default Feed;
