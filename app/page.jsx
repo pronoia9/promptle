38be975186b4272fb3302f628dac9c7e926ba4d9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-24 19:33:50 +0300

    update text

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <span className='orange_gradient text-center'> AI-Powered Prompts</span>
       </h1>
       <p className='desc text-center'>
-        Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        Promptle is an open-source AI prompting tool for modern world to discover, create and share creative prompts
       </p>
 
       {/* <Feed /> */}
