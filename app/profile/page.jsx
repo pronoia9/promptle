2160f37b4734ad71a23a9ae4b9b8ed034fa84d5a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-25 13:48:44 +0300

    change name to title

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -39,7 +39,7 @@ const MyProfile = () => {
 
   return (
     <Profile
-      name='My'
+      title='My'
       desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination.'
       data={myPosts}
       handleEdit={handleEdit}
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,10 +1,10 @@
 import { PromptCard } from '.';
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+const Profile = ({ title, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className='w-full'>
       <h1 className='head_text text-left'>
-        <span className='blue_gradient'>{name} Profile</span>
+        <span className='blue_gradient'>{title} Profile</span>
       </h1>
       <p className='desc text-left'>{desc}</p>
 
