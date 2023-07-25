ec390852b42b6ba29b96c7db5a618e0219c2fe12
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-07-25 14:43:32 +0300

    add filter functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -9,11 +9,28 @@ const Feed = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
-  const filterPrompts = () => {};
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, 'i'); // 'i' flag for case-insensitive search
+    return allPosts.filter((item) => regex.test(item.creator.username) || regex.test(item.tag) || regex.test(item.prompt));
+  };
 
-  const handleSearchChange = (e) => {};
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
 
-  const handleTagClick = () => {};
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   // Fetch posts
   useEffect(() => {
